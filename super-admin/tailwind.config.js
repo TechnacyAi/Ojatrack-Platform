2c73c3607229b06deb30d7cb841228269f28389f
TECHNACY <socialtechnacy@gmail.com>
2026-08-07 20:56:41 +0100

    Add Tailwind CSS configuration for custom theme

--- super-admin/tailwind.config.js
+++ super-admin/tailwind.config.js
@@ -1,1 +1,18 @@
-
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        oja: { black: '#050507', green: '#00FF88' }
+      },
+      fontFamily: {
+        grotesk: ['Space Grotesk', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
